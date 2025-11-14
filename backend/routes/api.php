e8883ece609ed16d60c2dd36f5c0d435ac6666e5
ono <dccess.f901ic.e.x@gmail.com>
2025-11-14 16:57:55 +0900

    apiアップデート

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,9 +13,12 @@ use App\Http\Controllers\TaskController;
 //     );
 // });/
 
-Route::get('/tasks',[TaskController::class, 'index']);
-Route::post('/tasks',[TaskController::class, 'store']);
-Route::put('/tasks/{id}', [TaskController::class, 'update']);
-Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+// Route::get('/tasks',[TaskController::class, 'index']);
+// Route::post('/tasks',[TaskController::class, 'store']);
+// Route::put('/tasks/{id}', [TaskController::class, 'update']);
+// Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+
+Route::resource('tasks', TaskController::class);
+
 
 
