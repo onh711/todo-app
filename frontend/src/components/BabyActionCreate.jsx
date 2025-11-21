6c3f45938ebc7c2068ed7e9a7b4b424f4cb4761e
ono <dccess.f901ic.e.x@gmail.com>
2025-11-21 16:40:28 +0900

    アクション登録機能更新

--- frontend/src/components/BabyActionCreate.jsx
+++ frontend/src/components/BabyActionCreate.jsx
@@ -10,7 +10,7 @@ export const BabyActionCreate = ({fetch}) => {
     }
   }
 
-  const handleSubmit = async(actionNum) =>{
+  const fetchAction = async(actionNum) =>{
     const API_URL = "http://localhost/api/dashbord"
     try {
       await axios.post(API_URL, babyActionCreate(actionNum));
@@ -23,13 +23,13 @@ export const BabyActionCreate = ({fetch}) => {
   return (
     <>
     <div>
-        <button onClick={()=>handleSubmit(1)}>寝る</button>
-        <button onClick={()=>handleSubmit(2)}>起きる</button>
-        <button onClick={()=>handleSubmit(3)}>授乳</button>
-        <button onClick={()=>handleSubmit(4)}>ご飯</button>
-        <button onClick={()=>handleSubmit(5)}>うんち</button>
-        <button onClick={()=>handleSubmit(6)}>おしっこ</button>
-        <button onClick={()=>handleSubmit(7)}>うんち/おしっこ</button>
+        <button onClick={()=>fetchAction(1)}>寝る</button>
+        <button onClick={()=>fetchAction(2)}>起きる</button>
+        <button onClick={()=>fetchAction(3)}>授乳</button>
+        <button onClick={()=>fetchAction(4)}>ご飯</button>
+        <button onClick={()=>fetchAction(5)}>うんち</button>
+        <button onClick={()=>fetchAction(6)}>おしっこ</button>
+        <button onClick={()=>fetchAction(7)}>うんち/おしっこ</button>
     </div>
     </>
   )
