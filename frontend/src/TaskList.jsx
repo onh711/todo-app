57ca1f7146e9820c4c8d9df522a74ca804493f05
onh711 <fmv811@yahoo.co.jp>
2025-12-06 16:38:29 +0900

    タスク一覧修正

--- frontend/src/TaskList.jsx
+++ frontend/src/TaskList.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import { TaskFilter } from "./components/TaskFilter";
+import React, { useEffect, useState } from 'react';
+import axios from './api/axios';
+
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { TaskFilter } from './components/TaskFilter';
 
 export const TaskList = () => {
   const [tasks, setTasks] = useState([]);
 
   const featchTasks = async () => {
     try {
-      const API_URL = "http://localhost/api/tasks";
+      const API_URL = '/api/tasks';
       const res = await axios.get(API_URL);
       setTasks(res.data.tasks);
     } catch (e) {
@@ -24,9 +25,9 @@ export const TaskList = () => {
 
   return (
     <>
-      <Box sx={{ margin: "0 auto", textAlign: "center" }}>
-        <Container sx={{ marginBottom: "30px" }}>
-          <Typography variant="h1" fontSize={40} sx={{ padding: "35px" }}>
+      <Box sx={{ margin: '0 auto', textAlign: 'center' }}>
+        <Container sx={{ marginBottom: '30px' }}>
+          <Typography variant="h1" fontSize={40} sx={{ padding: '35px' }}>
             タスク一覧
           </Typography>
           <TaskFilter tasks={tasks} onChange={featchTasks} />
