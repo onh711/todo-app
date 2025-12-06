19ff48c0035dcdfee042f1d8b24aadd5d9eb9879
onh711 <fmv811@yahoo.co.jp>
2025-12-06 22:24:23 +0900

    .env.example update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\BabyController;
 use App\Http\Controllers\BabyActionController;
 
 // Route::post('/register', [BabyController::class, 'store']);
