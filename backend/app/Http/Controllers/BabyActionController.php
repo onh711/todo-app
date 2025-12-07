1e0b4ec7d978e654355fde1f1d3fd65e9b73a59d
onh711 <fmv811@yahoo.co.jp>
2025-12-08 00:24:29 +0900

    AddProtectedRoute

--- backend/app/Http/Controllers/BabyActionController.php
+++ backend/app/Http/Controllers/BabyActionController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use App\Models\Baby;
 use App\Models\BabyAction;
-use Carbon\Carbon;
 
 class BabyActionController extends Controller
 {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { Register } from './components/Register';
 import { TaskList } from './components/TaskList';
 import { Header } from './components/Header';
 import { DashBoard } from './components/DashBoard';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -12,9 +13,24 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LoginForm />} />
+        <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/tasks" element={<TaskList />} />
-        <Route path="/dashboard" element={<DashBoard />}></Route>
+        <Route
+          path="/tasks"
+          element={
+            <ProtectedRoute>
+              <TaskList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashBoard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
